7ad1a2b259f462a329430db97adbb33d70f23de9
bLutz7269 <brycelutz6@gmail.com>
2022-10-31 12:37:21 -0700

    first commit

---
+++ microservice.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int numChecker(){
+    int input;
+
+    cin >> input;
+    while(!(input == 1 || input == 2)){
+        cout << "Error, invalid input. Try again: ";
+        cin.clear();
+        cin.ignore(100, '\n');
+        cin >> input;
+    }
+    return input;
+}
+
+char lightsError(){
+    char c;
+
+    cin >> c;
+    while(!(c == 'O' || c == 'W' || c == 'R' || c == 'G' || c == 'B')){
+        cout << "Error, invalid input. Try again: ";
+        cin.clear();
+        cin.ignore(100, '\n');
+        cin >> c;
+    }
+    return c;
+}
